b681e4b4b63d35d5eafc848e2393e5ad755dc549
Pitermaniak99 <piotr.kozub1999@gmail.com>
2023-04-21 23:53:18 +0200

    Bot level menu

--- main.py
+++ main.py
@@ -1,68 +1,13 @@
 import pygame
-
-from checkers.constants import GUI_WIDTH, GUI_HEIGHT, SQUARE_SIZE, BLACK, WHITE
+from RadioButton import RadioButton
+from checkers.constants import GUI_WIDTH, GUI_HEIGHT, SQUARE_SIZE, BLACK, WHITE, LIGHT_BLUE
 from checkers.game import Game
 from minimax.algorithm import minimax
+
 pygame.init()
 FPS = 60
-font = pygame.font.SysFont('Arial', 40, bold=True)
-
 WIN = pygame.display.set_mode((GUI_WIDTH, GUI_HEIGHT))
 pygame.display.set_caption('Warcaby')
-
-class Button():
-
-    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.onclickFunction = onclickFunction
-        self.onePress = onePress
-
-        self.fillColors = {
-            'normal': '#ffffff',
-            'hover': '#666666',
-            'pressed': '#333333',
-        }
-
-        self.buttonSurface = pygame.Surface((self.width, self.height))
-        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
-
-        self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
-
-        self.alreadyPressed = False
-
-
-    def process(self):
-        mousePos = pygame.mouse.get_pos()
-
-        self.buttonSurface.fill(self.fillColors['normal'])
-        if self.buttonRect.collidepoint(mousePos):
-            self.buttonSurface.fill(self.fillColors['hover'])
-
-            if pygame.mouse.get_pressed(num_buttons=3)[0]:
-                self.buttonSurface.fill(self.fillColors['pressed'])
-
-                if self.onePress:
-                    self.onclickFunction()
-
-                elif not self.alreadyPressed:
-                    self.onclickFunction()
-                    self.alreadyPressed = True
-
-            else:
-                self.alreadyPressed = False
-
-        self.buttonSurface.blit(self.buttonSurf, [
-            self.buttonRect.width / 2 - self.buttonSurf.get_rect().width / 2,
-            self.buttonRect.height / 2 - self.buttonSurf.get_rect().height / 2
-        ])
-        WIN.blit(self.buttonSurface, self.buttonRect)
-
-
-def myFunction(value):
-    print('MinMax depth = ' + str(value))
 
 
 def get_row_col_from_mouse(pos):
@@ -75,28 +20,46 @@ def get_row_col_from_mouse(pos):
 def main():
     run = True
     clock = pygame.time.Clock()
-    WIN.fill(WHITE)
+    WIN.fill(LIGHT_BLUE)
     game = Game(WIN)
-    objects = []
-    DEPTH = 2
-    Button1 = Button(1080, 70, 100, 50, '1', myFunction(1))
-    Button2 = Button(1080, 140, 100, 50, '2', myFunction(2), True)
-    objects.append(Button1)
-    objects.append(Button2)
+    bot_depth = 1
+
+    ##### BOT TEXT #####
+    font_bot = pygame.font.SysFont('Arial', 32, bold=True)
+    bot_text = font_bot.render("Wybierz poziom przeciwnika:", True, BLACK)
+    bot_textrect = bot_text.get_rect()
+    bot_textrect.center = ((1000, 50))
+
+    ##### BOT BUTTONS #####
+    font_button = pygame.font.SysFont(None, 40)
+    bot_buttons = [
+        RadioButton(850, 90, 125, 40, font_button, "AI 1"),
+        RadioButton(1025, 90, 125, 40, font_button, "AI 2"),
+        RadioButton(850, 155, 125, 40, font_button, "AI 3"),
+        RadioButton(1025, 155, 125, 40, font_button, "AI 4"),
+        RadioButton(937, 220, 125, 40, font_button, "AI 5")
+    ]
+    for rb in bot_buttons:
+        rb.set_radio_buttons(bot_buttons)
+    bot_buttons[0].clicked = True
+    group = pygame.sprite.Group(bot_buttons)
+
+    ##### GAME LOOP #####
     while run:
         clock.tick(FPS)
+        WIN.blit(bot_text, bot_textrect)
 
         if game.turn == BLACK:
-            value, new_board = minimax(game.get_board(), DEPTH, BLACK, game)
+            value, new_board = minimax(game.get_board(), bot_depth, BLACK, game)
             game.ai_move(new_board)
-        for object in objects:
-            object.process()
-        pygame.display.flip()
+            print("Bot wykonał ruch z głębią = " + str(bot_depth))
+
         if game.winner() != None:
             print(game.winner())
             run = False
 
-        for event in pygame.event.get():
+        event_list = pygame.event.get()
+        for event in event_list:
             if event.type == pygame.QUIT:
                 run = False
 
@@ -105,6 +68,21 @@ def main():
                 if pos[0] < 800:
                     row, col = get_row_col_from_mouse(pos)
                     game.select(row, col)
+
+        if bot_buttons[0].checkClick():
+            bot_depth = 1
+        if bot_buttons[1].checkClick():
+            bot_depth = 2
+        if bot_buttons[2].checkClick():
+            bot_depth = 3
+        if bot_buttons[3].checkClick():
+            bot_depth = 4
+        if bot_buttons[4].checkClick():
+            abot_depth = 5
+
+        group.update(event_list)
+        group.draw(WIN)
+        pygame.display.flip()
 
         game.update()
 
